801e82ff54cf42984ba9271f2673526e053c5c66
Fylan <56334@163.com>
2024-11-22 18:10:06 +0800

    update getStudentLatenciesInfoByStudentLatenciesInfo

--- src/main/java/com/example/ssmdemo22/demos/mapper/StudentLatenciesInfoMapper.java
+++ src/main/java/com/example/ssmdemo22/demos/mapper/StudentLatenciesInfoMapper.java
@@ -2,6 +2,15 @@ package com.example.ssmdemo22.demos.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.ssmdemo22.demos.entity.StudentLatenciesInfo;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 public interface StudentLatenciesInfoMapper extends BaseMapper<StudentLatenciesInfo> {
+
+    @Insert("insert into student_latencies_info (student_id,latencies,latencies_time) values (#{studentId},#{latencies},#{latenciesTime})")
+    public int insertStudentLatenciesInfo(StudentLatenciesInfo studentLatenciesInfo);
+
+    public List<StudentLatenciesInfo> getStudentLatenciesInfoByStudentLatenciesInfo(StudentLatenciesInfo studentLatenciesInfo);
 }
--- src/main/java/com/example/ssmdemo22/demos/mapper/SysDepartmentMapper.java
+++ src/main/java/com/example/ssmdemo22/demos/mapper/SysDepartmentMapper.java
@@ -2,7 +2,6 @@ package com.example.ssmdemo22.demos.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.ssmdemo22.demos.entity.SysDepartment;
-import org.apache.ibatis.annotations.Mapper;
 
 public interface SysDepartmentMapper extends BaseMapper<SysDepartment> {
 }
